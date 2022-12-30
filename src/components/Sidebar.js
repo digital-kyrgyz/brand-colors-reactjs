565acbe9b891be4c8bab90b31a0d0285a4d3a6db
Melis Archabaev <melis.archabaev.kg@gmail.com>
2022-12-30 17:54:24 +0600

    Added SCSS

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,25 +1,59 @@
 import { useState } from "react";
+import Modal from "react-modal";
+import { GrClose } from "react-icons/gr";
 
 function Sidebar(props) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const toggleModal = () => {
+    console.log("Before: " + modalIsOpen);
+    let me = !modalIsOpen;
+    console.log(me);
+    setModalIsOpen(me);
+  };
+
   return (
     <>
-      <div className="logo">
-        <a href="#">
-          Brand<b>Colors</b>
-        </a>
-      </div>
-      <div className="description">
-        The biggest collection of official brand color codes around.Created by
-        Me and friends.
-      </div>
-      <nav className="menu">
-        <ul>
-          <li>
-            <a href="#"> About BrandColors</a>
-          </li>
-        </ul>
-      </nav>
-      
+      <aside className="sidebar">
+        <div className="logo">
+          <a href="#">
+            Brand<b>Colors</b>
+          </a>
+        </div>
+        <div className="description">
+          The biggest collection of official brand color codes around.Created by
+          Me and friends.
+        </div>
+        <nav className="menu">
+          <ul>
+            <li>
+              {/* {console.log(modalIsOpen)} */}
+              <a onClick={toggleModal}>{modalIsOpen} About BrandColors</a>
+            </li>
+          </ul>
+        </nav>
+      </aside>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={toggleModal}
+        className="about-modal"
+        overlayClassName="about-modal-overlay"
+      >
+        <button className="modal-close-btn" onClick={toggleModal}>
+          <GrClose />
+        </button>
+        <h3>About BrandColors</h3>
+        <p>
+          BrandColors was created by DesignBombs. The goal was to create a
+          helpful reference for the brand color codes that are needed most
+          often.
+        </p>
+        <p>
+          It's been featured by Smashing Magazine, CSS-Tricks, Web Design Depot,
+          Tuts+, and over 2 million pageviews. There are now over 600 brands
+          with 1600 colors and the collection is always growing.
+        </p>
+      </Modal>
     </>
   );
 }
